c5a3731fb27cd1cfb067b6acdec5182b9107fa6c
Alysson Chicó <alysson.chicoh@gmail.com>
2018-12-09 18:12:33 -0200

    Primeira Função Definida

--- Códigos/Primeira_Funcao.hs
+++ Códigos/Primeira_Funcao.hs
@@ -1,1 +1,8 @@
+--AQUI DEFINIREMOS NOSSA PRIMEIRA FUNÇÃO
 doubleMe x = x + x
+
+--AQUI A SEGUNDA QUE RECEBE A PRIMEIRA
+doubleUs x y = doubleMe x + doubleMe y
+
+--AQUI VAMOS DEFINIR UMA FUNCAO COM CONDICAO
+doubleSmallNumber x = if x > 100 then x else x*2
